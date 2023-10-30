40d5b5e642b1fc8557b71b5531225eef9f1f22e1
Nhihoang39 <145347002+Nhihoang39@users.noreply.github.com>
2023-10-30 11:22:34 +0700

    Add files via upload

---
+++ js/custom2.js
@@ -0,0 +1,44 @@
+$("#banner").owlCarousel({
+    loop:true, // cho lap lại
+    items:1, // xuất hiện 1 ảnh thôi
+    smartSpeed:180, // tốc độ thay đổi ảnh
+    autoplay:true, // cho phép tự động chạy
+    autoplayTimeout:3200, // thời gian chờ khi chuyển ảnh khi chạy tự động
+    nav:false, // cho xuất hiện bộ nút tới lui
+    margin:0,
+    dots:false,
+    rtl:true,
+    autoHeight:true
+
+    });
+
+    $("#banner2").owlCarousel({
+        loop:true, // cho lap lại
+        items:1, // xuất hiện 1 ảnh thôi
+        smartSpeed:180, // tốc độ thay đổi ảnh
+        autoplay:true, // cho phép tự động chạy
+        autoplayTimeout:3200, // thời gian chờ khi chuyển ảnh khi chạy tự động
+        nav:false, // cho xuất hiện bộ nút tới lui
+        margin:0,
+        dots:false,
+        rtl:true,
+        autoHeight:true,
+       
+        })
+    
+        $(window).scroll(function(){
+            $('nav').toggleClass('scrolled', $(this).scrollTop() > 500);
+            });
+
+$("#text-slide").owlCarousel({
+        loop:true, // cho lap lại
+        items:1, // xuất hiện 1 ảnh thôi
+        smartSpeed:180, // tốc độ thay đổi ảnh
+        autoplay:true, // cho phép tự động chạy
+        autoplayTimeout:3200, // thời gian chờ khi chuyển ảnh khi chạy tự động
+        nav:false, // cho xuất hiện bộ nút tới lui
+        margin:0,
+        dots:false,
+        
+       
+        })
